729042d0f13c73ff1421a77f8e831af3ee51a5d3
Francisco Saldaña <pako7797@icloud.com>
2018-09-01 23:50:54 -0500

    Matrix errors fixed

--- src/fashion_show/Fashion_Show.java
+++ src/fashion_show/Fashion_Show.java
@@ -1,7 +1,10 @@
 package fashion_show;
 
 import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Fashion_Show {
 
@@ -12,22 +15,21 @@ public class Fashion_Show {
 
 
 
-    public  static  void main (String args[]){
+    public  static  void main (String args[])throws FileNotFoundException, IOException{
+        FileReader f = new FileReader("test.in");
+        BufferedReader b = new BufferedReader(f);
         Scanner entradaEscaner = new Scanner (System.in);
         int t;
         int n, m;
         String data[],temp_line;
-        System.out.print("#Test Cases: ");
+        t = Integer.parseInt(b.readLine());
 
-        t = entradaEscaner.nextInt();
 
-        entradaEscaner.nextLine();
 
         for (int i=0; i < t; i++){
 
-            System.out.print("give me N and M: ");
 
-            temp_line = entradaEscaner.nextLine();
+            temp_line = b.readLine();
 
             data = temp_line.split(" ");
 
@@ -35,16 +37,47 @@ public class Fashion_Show {
 
             m = Integer.parseInt(data[1]);
 
-            Models mo = new Models(n,m);
+            char matrix [][] = new char[n][n];
 
+            matrix = fillMatrix(matrix,n);
+
+            int pos_i, pos_j;
+            char type_model;
+
+            for (int j = 0; j <m; j++){
+
+                temp_line = b.readLine();
+                data = temp_line.split(" ");
+                type_model = data[0].charAt(0);
+                pos_i = Integer.parseInt(data[1]);
+                pos_j = Integer.parseInt(data[2]);
+
+                matrix[pos_i - 1][pos_j -1] = type_model;
+
+
+            }
+
+            Models mo = new Models(n,m,matrix);
 
             mo.printFashionMatrix();
 
 
         }
 
+        b.close();
 
 
+
+    }
+
+    public static  char[][] fillMatrix(char [][]matrix, int n){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                matrix [i][j] = '.';
+            }
+        }
+
+        return matrix;
     }
 
 
--- src/fashion_show/Models.java
+++ src/fashion_show/Models.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Models {
 
-    private int n, m, maxT;
+    private int n, m;
 
     private String temp_line;
 
@@ -16,23 +16,16 @@ public class Models {
 
 
 
-    public  Models(int n, int m){
+    public  Models(int n, int m, char[][] matrix){
         this.m = m;
         this.n = n;
-        this.fashion_matrix = new char [n] [n] ;
-        this.fillMatrix();
-        this.getModelsPositions();
+        this.fashion_matrix = matrix;
+        //this.getModelsPositions();
 
 
     }
 
-    public void fillMatrix(){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                fashion_matrix [i][j] = '.';
-            }
-        }
-    }
+
 
     public void printFashionMatrix() {
 
@@ -47,104 +40,190 @@ public class Models {
     public void getModelsPositions(){
 
 
-        int pos_i, pos_j;
-        char type_model;
 
-        for (int i = 1; i <=m; i++){
-            System.out.print("Give me Model Type(+,x,o), i Position, j Position : ");
-            this.temp_line = this.entradaEscaner.nextLine();
-            this.data = this.temp_line.split(" ");
-            type_model = data[0].charAt(0);
-            pos_i = Integer.parseInt(data[1]);
-            pos_j = Integer.parseInt(data[2]);
+        checkO(n-1,n-1);
 
-            this.fashion_matrix[pos_i - 1][pos_j -1] = type_model;
+        checkPlus(n-1,n-1);
 
+        checkX(n-1,n-1);
 
-        }
-        this.maxT = 0;
-        checkO(0,0);
 
     }
 
-    public boolean checkO(int i, int j){
+    public void checkO(int i, int j){
 
         int count = 0;
 
+        if (validation(i,j) ){
 
-        if (validation(i,j) && this.maxT< (n*n)){
-
-            this.maxT++;
-            System.out.println(i+ " "+j);
-
-            if (validation(i-1,j-1) && (this.fashion_matrix[i-1][j-1] == 'x' || this.fashion_matrix[i-1][j-1] == '.' ) ){
-                checkO(i-1,j-1);
+            if (validation(i-1,j-1) && (this.fashion_matrix[i-1][j-1] == 'x' || this.fashion_matrix[i-1][j-1] == '.'  ) ){
                 count++;
-
             }
-
             if (validation(i+1,j-1) && ( this.fashion_matrix[i+1][j-1] == 'x' || this.fashion_matrix[i+1][j-1] == '.' ) ){
-                checkO(i+1,j-1);
                 count++;
-
             }
             if (validation(i+1,j+1) && (this.fashion_matrix[i+1][j+1] == 'x' || this.fashion_matrix[i+1][j+1] == '.' )){
-                checkO(i+1,j+1);
                 count++;
-
-
             }
             if (validation(i-1,j+1) && (this.fashion_matrix[i-1][j+1] == 'x' || this.fashion_matrix[i-1][j+1] == '.')){
-                checkO(i-1,j+1);
                 count++;
-
-
-
             }
-
             if (validation(i-1,j) && (this.fashion_matrix[i-1][j] == '+' || this.fashion_matrix[i-1][j] == '.' ) ){
-                checkO(i-1,j);
                 count++;
-
             }
-
             if (validation(i,j-1) && (this.fashion_matrix[i][j-1] == '+' || this.fashion_matrix[i][j-1] == '.' ) ){
-                checkO(i,j-1);
                 count++;
-
             }
-
             if (validation(i+1,j) && (this.fashion_matrix[i+1][j] == '+' || this.fashion_matrix[i+1][j] == '.' ) ){
-                checkO(i+1,j);
                 count++;
-
+            }
+            if (validation(i,j+1) && (this.fashion_matrix[i][j+1] == '+' || this.fashion_matrix[i][j+1] == '.'  )){
+                count++;
             }
 
-            if (validation(i,j+1) && (this.fashion_matrix[i][j+1] == '+' || this.fashion_matrix[i][j+1] == '.'  )){
-                checkO(i,j+1);
+            if(!validation(i-1,j-1)){
                 count++;
+            }
 
+            if(!validation(i+1,j-1) ){
+                count++;
+            }
+            if(!validation(i+1,j+1)  ){
+                count++;
+            }
+            if(!validation(i-1,j+1) ){
+                count++;
+            }
+            if(!validation(i-1,j) ){
+                count++;
+            }
 
+            if(!validation(i,j-1) ){
+                count++;
+            }
+            if(!validation(i+1,j) ){
+                count++;
+            }
+            if(!validation(i,j+1) ){
+                count++;
             }
 
             if(count == 8){
-
                 this.fashion_matrix[i][j] = 'o';
-
-
             }
 
+            if(j == 0){
+                i--;
+                j = n-1;
+            }else{
+                j--;
+            }
+
+            this.checkO(i,j);
+        }
+
+    }
+
+    public void checkPlus(int i, int j){
+
+        int count = 0;
+
+        if (validation(i,j)  ){
+
+            if (validation(i-1,j-1) && this.fashion_matrix[i][j]=='.' && (this.fashion_matrix[i-1][j-1] == 'x' || this.fashion_matrix[i-1][j-1] == '.' ) ){
+                count++;
+
+            }
+            if (validation(i+1,j-1) && this.fashion_matrix[i][j]=='.' &&( this.fashion_matrix[i+1][j-1] == 'x' || this.fashion_matrix[i+1][j-1] == '.' ) ){
+                count++;
+            }
+            if (validation(i+1,j+1) && this.fashion_matrix[i][j]=='.' &&(this.fashion_matrix[i+1][j+1] == 'x' || this.fashion_matrix[i+1][j+1] == '.' )){
+                count++;
+            }
+            if (validation(i-1,j+1) && this.fashion_matrix[i][j]=='.' && (this.fashion_matrix[i-1][j+1] == 'x' || this.fashion_matrix[i-1][j+1] == '.')){
+                count++;
+            }
+            if(!validation(i-1,j-1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+
+            if(!validation(i+1,j-1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+            if(!validation(i+1,j+1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+            if(!validation(i-1,j+1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+
+            if(count == 4){
+                this.fashion_matrix[i][j] = '+';
+            }
+
+            if(j == 0){
+                i--;
+                j = n-1;
 
 
+            }else{
+                j--;
+            }
+
+            this.checkPlus(i,j);
+        }
+    }
+
+    public void checkX(int i, int j){
+
+        int count = 0;
+
+        if (validation(i,j)  ){
+
+            if (validation(i-1,j) && this.fashion_matrix[i][j]=='.' &&  (this.fashion_matrix[i-1][j] == '+' || this.fashion_matrix[i-1][j] == '.'  ) ){
+                count++;
+            }
+
+            if (validation(i,j-1) && this.fashion_matrix[i][j]=='.' &&  (this.fashion_matrix[i][j-1] == '+' || this.fashion_matrix[i][j-1] == '.' ) ){
+                count++;
+            }
+
+            if (validation(i+1,j) && this.fashion_matrix[i][j]=='.' &&   (this.fashion_matrix[i+1][j] == '+' || this.fashion_matrix[i+1][j] == '.' ) ){
+                count++;
+            }
+
+            if (validation(i,j+1) && this.fashion_matrix[i][j]=='.' &&   (this.fashion_matrix[i][j+1] == '+' || this.fashion_matrix[i][j+1] == '.'  ) ){
+                count++;
+            }
+            if(!validation(i-1,j) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+            if(!validation(i,j-1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+            if(!validation(i+1,j) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+            if(!validation(i,j+1) && this.fashion_matrix[i][j]=='.'){
+                count++;
+            }
+
+            if(count == 4){
+                this.fashion_matrix[i][j] = 'x';
+            }
+            if(j == 0){
+                i--;
+                j = n-1;
 
 
+            }else{
+                j--;
+            }
 
-
+            this.checkX(i,j);
 
         }
 
-
-        return true;
     }
 
     public boolean validation(int i, int j){
@@ -159,19 +238,5 @@ public class Models {
 
     }
 
-    public void setM(int m) {
-        this.m = m;
-    }
 
-    public void setN(int n) {
-        this.n = n;
-    }
-
-    public int getM() {
-        return m;
-    }
-
-    public int getN() {
-        return n;
-    }
 }
